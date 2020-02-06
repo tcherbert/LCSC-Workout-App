92c8a40d9cd7f159e2851aded063e063e06a5119
Tim Herbert <timherbert@Tims-MacBook-Pro.local>
2020-02-05 16:46:48 -0800

    Super buggy first deployment on ios and android devices.

--- LCSCWorkoutApp/src/pages/Home.tsx
+++ LCSCWorkoutApp/src/pages/Home.tsx
@@ -1,26 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonImg } from '@ionic/react';
 import React from 'react';
+
+
+
+
+
+const catImage = 'http://placekitten.com/g/200/300';
+
+
+console.log(catImage);
+
+
+
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle><IonImg className="logo" src="http://isoptera.lcsc.edu/~tcherbert/cs480/gwlogo.png" /></IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <header>
+            {/* <img src="images/Logo_screenDPI-01.png" alt="Gym warrior Logo" className="center" /> */}
+            <IonImg className="center" src="http://isoptera.lcsc.edu/~tcherbert/cs480/landing_logo.png" />
+        </header>
+
+        <main>
+            <section>
+                <form>
+                  <IonInput type="text" placeholder="Enter Username" name="uname" required />
+                  <IonInput type="password" placeholder="Enter Password" name="psw" required />
+                  <button type="submit">Login</button>
+                </form>
+            </section>
+        </main>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
+
