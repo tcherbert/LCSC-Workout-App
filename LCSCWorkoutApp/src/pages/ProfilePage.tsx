4f42b45518f3659144637911f21a2c5d7162b11b
tcarver371 <39978599+tcarver371@users.noreply.github.com>
2020-02-12 02:00:49 -0800

    Added Profile Page to the respository

---
+++ LCSCWorkoutApp/src/pages/ProfilePage.tsx
@@ -0,0 +1,40 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonNav, IonImg } from '@ionic/react';
+import React from 'react';
+import {faSearch, faBars, faGrinAlt} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import profilePic from '../images/profilePhoto.jpeg';
+
+
+const Profile: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+          <IonTitle><IonImg className="logo" src="http://isoptera.lcsc.edu/~tcherbert/cs480/gwlogo.png" />
+                <FontAwesomeIcon className="icon" icon ={faSearch} />
+                <FontAwesomeIcon className="icon" icon ={faBars} />
+                <FontAwesomeIcon className="icon" icon ={faGrinAlt} />
+
+          </IonTitle>
+          </IonToolbar>
+          
+        </IonHeader>
+        <IonContent className="ion-padding">
+    <main>
+      
+      <img className="profile responsive" height='500' src = {profilePic} ></img>
+      <IonNav id = "navbar" className="center">
+          <ul id ="nav">
+              <li id="about">About</li>
+              <li id = "photos">Photos</li>
+              <li id = "friends">Friends</li>
+              <li id = "schedule">Schedule</li>
+          </ul>
+      </IonNav>
+    </main>
+        </IonContent>
+      </IonPage>
+    );
+  };
+
+export default Profile;
